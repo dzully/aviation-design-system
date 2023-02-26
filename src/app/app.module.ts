20e040c779b836f6722065caa00d2e37be06923b
Mohamad Dzul Syakimin <dzulsyakimin61@gmail.com>
2023-02-26 21:02:52 +0800

    chore:Docker and hello world component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,25 +12,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardDetailBoxComponent } from './main/component/card-detail-box/card-detail-box.component';
 import { CardDetailComponent } from './main/component/card-detail/card-detail.component';
 import { GraphComponent } from './main/component/graph/graph.component';
 import { HeaderComponent } from './main/component/header/header.component';
 import { SidebarComponent } from './main/component/sidebar/sidebar.component';
 import { DashboardComponent } from './main/pages/dashboard/dashboard.component';
+import { HelloWorldComponent } from './main/pages/hello-world/hello-world.component';
+import { FooterComponent } from './main/pages/main/footer/footer.component';
 import { MainComponent } from './main/pages/main/main.component';
+import { GraphWavesComponent } from './main/pages/waves/graph-waves/graph-waves.component';
 import { WavesComponent } from './main/pages/waves/waves.component';
+import { CardBoxComponent } from './main/pages/weather/card-box/card-box.component';
 import { WeatherComponent } from './main/pages/weather/weather.component';
 import { WindComponent } from './main/pages/wind/wind.component';
-import { GraphWavesComponent } from './main/pages/waves/graph-waves/graph-waves.component';
-import { CardDetailBoxComponent } from './main/component/card-detail-box/card-detail-box.component';
-import { CardBoxComponent } from './main/pages/weather/card-box/card-box.component';
-import { FooterComponent } from './main/pages/main/footer/footer.component';
 
 const appRoutes: Routes = [
   { path: 'overview', component: MainComponent },
   { path: 'wind', component: WindComponent },
   { path: 'waves', component: WavesComponent },
   { path: 'weather', component: WeatherComponent },
+  { path: 'hellow-world', component: HelloWorldComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
@@ -50,6 +52,7 @@ const appRoutes: Routes = [
     CardDetailBoxComponent,
     CardBoxComponent,
     FooterComponent,
+    HelloWorldComponent,
   ],
   imports: [
     BrowserModule,
